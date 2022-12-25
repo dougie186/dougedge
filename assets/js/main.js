0da0cef50ebc999e307ba87dd85af7251a7e84b8
Doug Edge <120598872+dougie186@users.noreply.github.com>
2022-12-25 13:29:13 -0500

    Website

---
+++ assets/js/main.js
@@ -0,0 +1,141 @@
+"use strict";
+
+
+jQuery(document).ready(function ($) {
+
+	$(window).load(function () {
+		$(".loaded").fadeOut();
+		$(".preloader").delay(1000).fadeOut("slow");
+	});
+    /*---------------------------------------------*
+     * Mobile menu
+     ---------------------------------------------*/
+   
+    $('#navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+	
+	
+	/*---------------------------------------------*
+     * Portfolio Pop Up Animation
+     ---------------------------------------------*/
+
+    $('.portfolio-img').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+		});
+     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+          gallery: {
+              enabled: true
+            },
+		  type: 'iframe',
+    iframe: {
+        patterns: {
+            youtube: {
+                index: 'youtube.com/', 
+                id: function(url) {        
+                    var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+                    if ( !m || !m[1] ) return null;
+                    return m[1];
+                },
+                src: '//www.youtube.com/embed/%id%?autoplay=1'
+            },
+            vimeo: {
+                index: 'vimeo.com/', 
+                id: function(url) {        
+                    var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+                    if ( !m || !m[5] ) return null;
+                    return m[5];
+                },
+                src: '//player.vimeo.com/video/%id%?autoplay=1'
+            }
+        }
+    }
+});
+	
+	
+	/*---------------------------------------------*
+     * Menu Background Change
+     ---------------------------------------------*/
+	
+	var windowWidth = $(window).width();
+    if (windowWidth > 757) {
+
+        
+          
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 500) {
+                    $('.navbar').fadeIn(500);
+                    $('.navbar').addClass('menu-bg');
+
+                } else {
+                    
+                    $('.navbar').removeClass('menu-bg');
+                }
+            });
+        
+    }
+	$('#bs-example-navbar-collapse-1').localScroll();
+
+
+    /*---------------------------------------------*
+     * STICKY scroll
+     ---------------------------------------------*/
+
+		$.localScroll();
+
+
+
+    /*---------------------------------------------*
+     * Counter 
+     ---------------------------------------------*/
+
+//    $('.statistic-counter').counterUp({
+//        delay: 10,
+//        time: 2000
+//    });
+
+
+
+
+    /*---------------------------------------------*
+     * WOW
+     ---------------------------------------------*/
+
+//        var wow = new WOW({
+//            mobile: false // trigger animations on mobile devices (default is true)
+//        });
+//        wow.init();
+
+
+    /* ---------------------------------------------------------------------
+     Carousel
+     ---------------------------------------------------------------------= */
+
+//    $('.testimonials').owlCarousel({
+//        responsiveClass: true,
+//        autoplay: false,
+//        items: 1,
+//        loop: true,
+//        dots: true,
+//        autoplayHoverPause: true
+//
+//    });
+
+
+    //End
+});
